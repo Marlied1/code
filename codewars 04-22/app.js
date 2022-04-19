9afe69ad53bf9aa9fe66b3a092a3767645dddf4d
Marlie Denis <marliedenis3@gmail.comgit config --global user.name Marlie>
2022-04-18 21:48:22 -0400

    new challenge added

--- codewars 04-22/app.js
+++ codewars 04-22/app.js
@@ -207,4 +207,18 @@ newArrayy.push(x[i]*2);
 }
 return newArrayy
 }
-//hey
+
+// Create a function that takes in an array.
+//If the first number, is less than the last number, 
+//alert "Hi". If the first number is greater than the 
+//last number, alert "Bye". If they are equal, alert
+//"We close in an hour".
+
+function checkFor(arr) {
+  if (arr[0] < arr [arr.length -1]) {
+      alert('HI!')
+  }else if (arr[0]>[arr.length -1]){
+      alert('Bye')
+  }alert('We close in an hour!')
+}
+
