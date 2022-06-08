c630f2a97ef4ece7cd21650a5a757848080c36d3
Marlie Denis <marliedenis3@gmail.comgit config --global user.name Marlie>
2022-06-08 00:21:23 -0400

    code

--- Even or Odd/js/main.js
+++ Even or Odd/js/main.js
@@ -6,3 +6,13 @@ for even numbers or "Odd" for odd numbers.
 //notes
 //crete a function
 //create a conditional statement that checks for even and odd 
+
+function even_or_odd(number) {
+  if(number % 2 ===0 ) {
+      return 'Even'
+  }else{
+      return 'Odd'
+  }
+}
+console.log(even_or_odd(2))
+//bllopers
