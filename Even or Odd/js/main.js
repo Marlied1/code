3441f5fabb6c7fde6d11fa23745f16f393076e21
Marlie Denis <marliedenis3@gmail.comgit config --global user.name Marlie>
2022-06-08 00:09:21 -0400

    update coding challenge

---
+++ Even or Odd/js/main.js
@@ -0,0 +1,8 @@
+/*
+Create a function that takes an integer as an argument and returns "Even" 
+for even numbers or "Odd" for odd numbers.
+*/
+
+//notes
+//crete a function
+//create a conditional statement that checks for even and odd 
