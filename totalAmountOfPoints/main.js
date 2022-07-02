c6d29537bd107a4289ea43d13e254800e4e2bed6
Marlie Denis <marliedenis3@gmail.comgit config --global user.name Marlie>
2022-07-02 05:49:56 -0400

    solution

--- totalAmountOfPoints/main.js
+++ totalAmountOfPoints/main.js
@@ -8,4 +8,12 @@
 //if x = y: 1 point
 
 
+function points(games) {
+    let score = games.map(g => {
+      let [x,y] = g.split(':')
+      return x>y ? 3 : x==y ? 1 : 0
+    }).reduce( (sum,val) => sum += val, 0)
+    return score
+  }
+
 
