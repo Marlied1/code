6325fb512c2d9e6bcff1197468af6cfabbbea600
Marlie Denis <marliedenis3@gmail.comgit config --global user.name Marlie>
2022-07-02 05:47:40 -0400

    codes

---
+++ totalAmountOfPoints/main.js
@@ -0,0 +1,11 @@
+//write a function that takes a collectiion and counts the points
+//of our team in the chapionship.
+
+//Rules for countinf points for each match:
+
+// if x > y: 3 points
+//if x < y: 0 point
+//if x = y: 1 point
+
+
+
